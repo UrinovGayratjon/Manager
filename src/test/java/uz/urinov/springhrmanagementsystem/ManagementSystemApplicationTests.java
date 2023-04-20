f36479ff003897108c6d276f85a518b57f4c2d2a
g.urinov <g.urinov@hlsys.uz>
2023-04-20 12:30:25 +0500

    finished

--- src/test/java/uz/urinov/springhrmanagementsystem/ManagementSystemApplicationTests.java
+++ src/test/java/uz/urinov/springhrmanagementsystem/ManagementSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringHrManagementSystemApplicationTests {
+class ManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
